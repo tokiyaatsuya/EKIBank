64e9d20906a23eae863ca1fb4cfc02d7c196186c
tokiyaatsuya <tokiya.atsuya@gmail.com>
2021-12-05 14:39:11 +0900

    Fix:質問

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,69 +2,69 @@ class QuestionsController < ApplicationController
 
   def show1
     @questions = Question.find(1,2,3)
-    @answers_1 = Q_01
-    @answers_2 = Q_02
-    @answers_3 = Q_03
+    @answers_1 = Answer.where(Q_01)
+    @answers_2 = Answer.where(Q_02)
+    @answers_3 = Answer.where(Q_03)
   end
 
   def show2
     @questions = Question.find(4,5)
-    @answers_5 = Q_05
+    @answers_5 = Answer.where(Q_05)
   end
 
   def show3
     @questions = Question.find(6,7)
-    @answers_7 = Q_07
+    @answers_7 = Answer.where(Q_07)
   end
 
   def show4
     @question = Question.find(8)
-    @answers_8 = Q_08
+    @answers_8 = Answer.where(Q_08)
   end
 
   def show5
     @question = Question.find(9)
-    @answers_9 = Q_09
+    @answers_9 = Answer.where(Q_09)
   end
 
   def show6
     @question = Question.find(10)
-    @answers_10 = Q_10
+    @answers_10 = Answer.where(Q_10)
   end
 
   def show7
     @question = Question.find(11)
-    @answers_11 = Q_11
+    @answers_11 = Answer.where(Q_11)
   end
 
   def show8
     @question = Question.find(12)
-    @answers_12 = Q_12
+    @answers_12 = Answer.where(Q_12)
   end
 
   def show9
     @question = Question.find(13)
-    @answers_13 = Q_13
+    @answers_13 = Answer.where(Q_13)
   end
 
   def show10
     @question = Question.find(14)
-    @answers_14 = Q_14
+    @answers_14 = Answer.where(Q_14)
   end
 
   def show11
     @question = Question.find(15)
-    @answers_15 = Q_15
+    @answers_15 = Answer.where(Q_15)
   end
 
   def show12
     @question = Question.find(16)
-    @answers_16 = Q_16
+    @answers_16 = Answer.where(Q_16)
   end
 
   def show13
     @question = Question.find(17)
-    @answers_17 = Q_17
+    @answers_17 = Answer.where(Q_17)
   end
 
   def create
