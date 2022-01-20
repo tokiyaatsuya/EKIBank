7bdc8af203f26d1a1006c680d9c65c6a67e89d1e
tokiyaatsuya <tokiya.atsuya@gmail.com>
2022-01-20 23:42:45 +0900

    Fix:questions_controller.rbへのコメント追加とscraping.rbのDRY化

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,13 @@
 class QuestionsController < ApplicationController
 
   def show1
+    # questionsテーブルからid:1,2,3,18を取得(18は追加項目なので飛び番号)
+    # id:1 = 家賃の予算は?
+    # id:2 = 購入する不動産の種別は？ 
+    # id:3 = 購入する不動産の予算は？
+    # id:18 = 希望する間取りは？
     @questions = Question.find(1,2,3,18)
+    # answersテーブルからquestion_id:1,2,3,18に紐付く選択肢を取得(18は追加項目なので飛び番号)
     @answers_1 = Answer.where(question_id: Q_01)
     @answers_2 = Answer.where(question_id: Q_02)
     @answers_3 = Answer.where(question_id: Q_03)
@@ -9,62 +15,100 @@ class QuestionsController < ApplicationController
   end
 
   def show2
+    # questionsテーブルからid:4,5を取得
+    # id:4 = 通勤通学の目的地の最寄駅は？
+    # id:5 = 通勤通学時の移動時間は?
     @questions = Question.find(4,5)
+    # answersテーブルからquestion_id:5に紐付く選択肢を取得、id:4は入力フォーム設置
     @answers_5 = Answer.where(question_id: Q_05)
   end
 
   def show3
+    # questionsテーブルからid:6,7を取得
+    # id:6 = 趣味等でよく行く場所の最寄駅は？
+    # id:7 = 趣味等でよく行く場所への移動時間は？
     @questions = Question.find(6,7)
+    # answersテーブルからquestion_id:7に紐付く選択肢を取得、id:6は入力フォーム設置
     @answers_7 = Answer.where(question_id: Q_07)
   end
 
   def show4
+    # questionsテーブルからid:8を取得
+    # id:8 = 駅前の雰囲気はどちらが好み？
     @question = Question.find(8)
+    # answersテーブルからquestion_id:8に紐付く選択肢YorNを取得
     @answers_8 = Answer.where(question_id: Q_08)
   end
 
   def show5
+    # questionsテーブルからid:9を取得
+    # id:9 = 駅前にスポーツジムは必須？
     @question = Question.find(9)
+    # answersテーブルからquestion_id:9に紐付く選択肢YorNを取得
     @answers_9 = Answer.where(question_id: Q_09)
   end
 
   def show6
+    # questionsテーブルからid:10を取得
+    # id:10 = 駅前にスターバックスコーヒーは必須？
     @question = Question.find(10)
+    # answersテーブルからquestion_id:10に紐付く選択肢YorNを取得
     @answers_10 = Answer.where(question_id: Q_10)
   end
 
   def show7
+    # questionsテーブルからid:11を取得
+    # id:11 = 駅前にマクドナルドは必須？
     @question = Question.find(11)
+    # answersテーブルからquestion_id:11に紐付く選択肢YorNを取得
     @answers_11 = Answer.where(question_id: Q_11)
   end
 
   def show8
+    # questionsテーブルからid:12を取得
+    # id:12 = 駅前に餃子の王将は必須？
     @question = Question.find(12)
+    # answersテーブルからquestion_id:12に紐付く選択肢YorNを取得
     @answers_12 = Answer.where(question_id: Q_12)
   end
 
   def show9
+    # questionsテーブルからid:13を取得
+    # id:13 = 駅前に食べログの評価が3.5以上のお店は必須？
     @question = Question.find(13)
+    # answersテーブルからquestion_id:13に紐付く選択肢YorNを取得
     @answers_13 = Answer.where(question_id: Q_13)
   end
 
   def show10
+    # questionsテーブルからid:14を取得
+    # id:14 = 駅前に23時まで営業しているスーパーマーケットは必須？
     @question = Question.find(14)
+    # answersテーブルからquestion_id:14に紐付く選択肢YorNを取得
     @answers_14 = Answer.where(question_id: Q_14)
   end
 
   def show11
+    # questionsテーブルからid:15を取得
+    # id:15 = 駅の近くに大きな公園は必須？
     @question = Question.find(15)
+    # answersテーブルからquestion_id:15に紐付く選択肢YorNを取得
     @answers_15 = Answer.where(question_id: Q_15)
   end
 
   def show12
+    # questionsテーブルからid:16を取得
+    # id:16 = 駅の近くに図書館は必須？
     @question = Question.find(16)
+    # answersテーブルからquestion_id:16に紐付く選択肢YorNを取得
     @answers_16 = Answer.where(question_id: Q_16)
   end
 
   def show13
+    # questionsテーブルからid:17を取得
+    # id:17 = 駅の近くに公衆浴場やサウナは必須?
     @question = Question.find(17)
+    # answersテーブルからquestion_id:17に紐付く選択肢YorNを取得
     @answers_17 = Answer.where(question_id: Q_17)
   end
 
--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -8,151 +8,53 @@ require 'selenium-webdriver'
   #end
 
 class Scraping
-  def self.click_rent_market_price_tokyo
+  def self.click_rent_market_price
+    # Seleniumの起動
     driver = Selenium::WebDriver.for :chrome
-    driver.get "https://suumo.jp/chintai/soba/tokyo/ensen/"
-    # xpathを使用したリンクの取得とクリック動作
-    elements = driver.find_elements(:xpath, '/html/body/div[4]/div/div[2]/div/div/div/div[2]/table/tbody//*[contains(@href, "tokyo")]')
-    urls = elements.map {|element| element.attribute('href')}
-    urls.each do |url|
-      driver.get(url)
-      driver.find_element(:xpath, '//*[@id="souba_madori-oneroom"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-1K/1DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-1LDK/2K/2DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-2LDK/3K/3DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-3LDK/4K"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
+    # 最初の遷移先の指定
+    driver.get "https://suumo.jp/chintai/soba/"
+    # 一都三県の家賃相場を調べる為に空の配列を用意
+    prefectures = []
+    # 関東エリアの中で東京、神奈川、埼玉、千葉のリンク(href)要素を持つものを繰り返し処理で全て取得してリンクを空の配列に格納する
+    driver.find_elements(:xpath, '//*[@id="js-areamenuKantoPanel"]/div/div[2]/ul//*[contains(@href, "tokyo")or contains(@href, "kanagawa")or contains(@href, "saitama")or contains(@href, "chiba")]').each do |post|
+      prefectures << post.attribute('href')
     end
-    return 'ok'
-  end
-
-  def self.click_rent_market_price_kanagawa
-    driver = Selenium::WebDriver.for :chrome
-    driver.get "https://suumo.jp/chintai/soba/kanagawa/ensen/"
-    # xpathを使用したリンクの取得とクリック動作
-    elements = driver.find_elements(:xpath, '/html/body/div[4]/div/div[2]/div/div/div/div[2]/table/tbody//*[contains(@href, "kanagawa")]')
-    urls = elements.map {|element| element.attribute('href')}
-    urls.each do |url|
-      driver.get(url)
-      driver.find_element(:xpath, '//*[@id="souba_madori-oneroom"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-1K/1DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-1LDK/2K/2DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-2LDK/3K/3DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-3LDK/4K"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
+    # 格納された一都三県の家賃相場を探すページを順に遷移する
+    prefectures.each do |prefecture|
+      driver.get(prefecture)
+      # 今回は「沿線・駅」の家賃相場を知りたいので、そのhref要素を取得してクリックさせる
+      driver.find_element(:xpath, '/html/body/div[4]/div/div[2]/div[1]/ul/li[1]/a').click
+      # 先のクリックにより一都三県のそれぞれが保持する沿線のリンクが表示されるので、その全てのhref要素を取得してelementsに格納する
+      elements = driver.find_elements(:xpath, '/html/body/div[4]/div/div[2]/div/div/div/div[2]/table/tbody//*[contains(@href, "tokyo")or contains(@href, "kanagawa")or contains(@href, "saitama")or contains(@href, "chiba")]')
+      # 格納されたelementsをmapメソッドで各沿線のhref要素のみを取り出す繰り返し処理を行いurlsに格納する
+      urls = elements.map {|element| element.attribute('href')}
+      # 格納された各沿線のhrefに対して順番に遷移を繰り返す
+      urls.each do |url|
+        driver.get(url)
+        # 間取りごとにラジオボタンの要素を取得してクリック
+        driver.find_element(:xpath, '//*[@id="souba_madori-oneroom"]').click
+        sleep(1)
+        # 間取りボタンをクリック後に更新ボタンをクリック
+        submit_btn = '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a'
+        driver.find_element(:xpath, submit_btn).click
+        sleep(1)
+        driver.find_element(:xpath, '//*[@id="souba_madori-1K/1DK"]').click
+        sleep(1)
+        driver.find_element(:xpath, submit_btn).click
+        sleep(1)
+        driver.find_element(:xpath, '//*[@id="souba_madori-1LDK/2K/2DK"]').click
+        sleep(1)
+        driver.find_element(:xpath, submit_btn).click
+        sleep(1)
+        driver.find_element(:xpath, '//*[@id="souba_madori-2LDK/3K/3DK"]').click
+        sleep(1)
+        driver.find_element(:xpath, submit_btn).click
+        sleep(1)
+        driver.find_element(:xpath, '//*[@id="souba_madori-3LDK/4K"]').click
+        sleep(1)
+        driver.find_element(:xpath, submit_btn).click
+        sleep(1)
+      end
     end
-    return 'ok'
-  end
-
-  def self.click_rent_market_price_saitama
-    driver = Selenium::WebDriver.for :chrome
-    driver.get "https://suumo.jp/chintai/soba/saitama/ensen/"
-    # xpathを使用したリンクの取得とクリック動作
-    elements = driver.find_elements(:xpath, '/html/body/div[4]/div/div[2]/div/div/div/div[2]/table/tbody//*[contains(@href, "saitama")]')
-    urls = elements.map {|element| element.attribute('href')}
-    urls.each do |url|
-      driver.get(url)
-      driver.find_element(:xpath, '//*[@id="souba_madori-oneroom"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-1K/1DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-1LDK/2K/2DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-2LDK/3K/3DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-3LDK/4K"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-    end
-    return 'ok'
-  end
-
-  def self.click_rent_market_price_chiba
-    driver = Selenium::WebDriver.for :chrome
-    driver.get "https://suumo.jp/chintai/soba/chiba/ensen/"
-    # xpathを使用したリンクの取得とクリック動作
-    elements = driver.find_elements(:xpath, '/html/body/div[4]/div/div[2]/div/div/div/div[2]/table/tbody//*[contains(@href, "chiba")]')
-    urls = elements.map {|element| element.attribute('href')}
-    urls.each do |url|
-      driver.get(url)
-      driver.find_element(:xpath, '//*[@id="souba_madori-oneroom"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-1K/1DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-1LDK/2K/2DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-2LDK/3K/3DK"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-      driver.find_element(:xpath, '//*[@id="souba_madori-3LDK/4K"]').click
-      sleep(1)
-      # 間取りボタンをクリック後に更新ボタンをクリック
-      driver.find_element(:xpath, '//*[@id="js-graphpanel-form"]/div/div[2]/div/div[1]/a').click
-      sleep(1)
-    end
-    return 'ok'
   end
 end
