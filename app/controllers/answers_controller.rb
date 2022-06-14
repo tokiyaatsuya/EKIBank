3f87ae06102493b665afe26d3455d2fa8b4db9aa
tokiyaatsuya <tokiya.atsuya@gmail.com>
2022-06-15 01:01:49 +0900

    Add:GooglePlacesAPIを使用し図書館のデータを取得・保存・表示、answers/search.html.erbにスクロールバーの追加

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -15,7 +15,7 @@ class AnswersController < ApplicationController
     @ohsho = session[:ohsho] == "必須" ? "有り" : ["有り", "無し"]
     @supermarket = session[:supermarket]
     @large_park = session[:large_park] == "必須" ? "有り" : ["有り", "無し"]
-    @library = session[:library]
+    @library = session[:library] == "必須" ? "有り" : ["有り", "無し"]
     @spa = session[:spa]
     # show_4の住みたいエリアの入力から条件分岐でスクレイピングのメソッドを始動する
     # 格納した値をmodelへ渡す為にグローバル変数を設定
@@ -44,7 +44,7 @@ class AnswersController < ApplicationController
       # uniq!メソッドで配列内の重複要素を除去し直す
       infomations = RentMarketPrice.where(station_name: $candidate_station.uniq!).and(
                     RentMarketPrice.where("(market_price <= ?) AND (floor_plan = ?)", @rent_budget, @floor_plan)).and(
-                    RentMarketPrice.where(gym: @gym, starbucks_coffee: @starbucks_coffee, mcdonalds: @mcdonalds, ohsho: @ohsho, large_park: @large_park))
+                    RentMarketPrice.where(gym: @gym, starbucks_coffee: @starbucks_coffee, mcdonalds: @mcdonalds, ohsho: @ohsho, large_park: @large_park, library: @library))
       @exactly_station = []
       # db参照の結果を出力する
       infomations.each do |info|
