e1365ca4095ad62377fdf98b332be96abc7cc018
tokiyaatsuya <tokiya.atsuya@gmail.com>
2022-07-03 17:22:35 +0900

    Add:GooglePlacesAPIを使用し23時以降も営業しているスーパーのデータを取得・保存・表示する

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -13,7 +13,7 @@ class AnswersController < ApplicationController
     @starbucks_coffee = session[:starbucks_coffee] == "必須" ? "有り" : ["有り", "無し"]
     @mcdonalds = session[:mcdonalds] == "必須" ? "有り" : ["有り", "無し"]
     @ohsho = session[:ohsho] == "必須" ? "有り" : ["有り", "無し"]
-    @supermarket = session[:supermarket]
+    @supermarket = session[:supermarket] == "必須" ? "有り" : ["有り", "無し"]
     @large_park = session[:large_park] == "必須" ? "有り" : ["有り", "無し"]
     @library = session[:library] == "必須" ? "有り" : ["有り", "無し"]
     @spa = session[:spa]
@@ -44,7 +44,7 @@ class AnswersController < ApplicationController
       # uniq!メソッドで配列内の重複要素を除去し直す
       infomations = RentMarketPrice.where(station_name: $candidate_station.uniq!).and(
                     RentMarketPrice.where("(market_price <= ?) AND (floor_plan = ?)", @rent_budget, @floor_plan)).and(
-                    RentMarketPrice.where(gym: @gym, starbucks_coffee: @starbucks_coffee, mcdonalds: @mcdonalds, ohsho: @ohsho, large_park: @large_park, library: @library))
+                    RentMarketPrice.where(gym: @gym, starbucks_coffee: @starbucks_coffee, mcdonalds: @mcdonalds, ohsho: @ohsho, supermarket: @supermarket, large_park: @large_park, library: @library))
       @exactly_station = []
       # db参照の結果を出力する
       infomations.each do |info|
