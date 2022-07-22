6bb8fb2162d780c02e3d3f78d75e9224e0bd641d
tokiyaatsuya <tokiya.atsuya@gmail.com>
2022-07-22 20:56:58 +0900

    Add:Basic認証機能を追加

--- app/webpacker/src/javascript/show_12.js
+++ app/webpacker/src/javascript/show_12.js
@@ -4,10 +4,10 @@ $(function(){
   $('#loading').hide();
   // submitボタンが実行されたタイミングで動作を起こしloading画面を表示する
   $('form').submit(function(e) {
-    // loading画面の表示インターバルを45秒として、45秒後は　loading画面を非表示にし、RESULTボタンの表示をリトライと変更する
+    // loading画面の表示インターバルを60秒として、60秒後は　loading画面を非表示にし、RESULTボタンの表示をリトライと変更する
     $('#loading').show(), setInterval(function() {
       $('#loading').hide();
       $('#result_btn').val('リトライ');
-    }, 45000);
+    }, 60000);
   });
 });
