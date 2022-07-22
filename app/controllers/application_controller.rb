bf73f7c84b5a1b5fb0a4a9c6d35d3193d645bec4
tokiyaatsuya <tokiya.atsuya@gmail.com>
2022-07-22 23:05:17 +0900

    Fix:Basic認証の導入に合わせルーティングの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    pages_show_path
+    root_path
   end
 
 #質問IDを定数化
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 
